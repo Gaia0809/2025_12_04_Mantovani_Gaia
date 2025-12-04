5635d237c0065160cbe442104c8877113acf18c6
Gaia0809 <gaia.mantovani.24@stud.itsaltoadriatico.it>
2025-12-04 09:35:33 +0100

    dart create

---
+++ primoesame/bin/esame.dart
@@ -0,0 +1,5 @@
+import 'package:esame/esame.dart' as esame;
+
+void main(List<String> arguments) {
+  print('Hello world: ${esame.calculate()}!');
+}
---
+++ primoesame/test/esame_test.dart
@@ -0,0 +1,8 @@
+import 'package:esame/esame.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
